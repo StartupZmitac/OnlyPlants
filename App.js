e6d80e2e8fa286dab043b799ec611c977ead7871
Aleksandra Kyc <ola.kyc@gmail.com>
2023-04-02 17:15:04 +0200

    dziala jak natura chciala

--- App.js
+++ App.js
@@ -1,10 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
 
 export default function App() {
+  let [isLoading, setIsLoading] = useState(true);
+  let [error, setError] = useState();
+  let [response, setResponse] = useState();
+
+  useEffect(() => {
+    const apiKey = 'sk-ROVe642993dacc8c4178';
+    const name = 'monstera';
+    fetch(`https://perenual.com/api/species-list?q=${name}&key=${apiKey}`)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setIsLoading(false);
+          setResponse(result);
+        },
+        (error) => {
+          setIsLoading(false);
+          setError(error);
+        }
+      )
+  }, []);
+
+  const getContent = () => {
+    if (isLoading) {
+      return <ActivityIndicator size="large" />;
+    }
+
+    if (error) {
+      return <Text>{error}</Text>
+    }
+    
+    if(typeof(response)!== 'undefined'){
+      console.log(response);
+      return <Text>{response.data[0].common_name}</Text>;
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>kocham piwo</Text>
+      {getContent()}
       <StatusBar style="auto" />
     </View>
   );
