ea0584638edd5444da0fd4401ea8ec22f52fa23a
mikolip716 <mikolip716@student.polsl.pl>
2023-04-17 18:11:16 +0200

    Working adding to database

--- database/CustomPlant.js
+++ database/CustomPlant.js
@@ -3,47 +3,9 @@ import {Text, TextInput, View, Button} from 'react-native';
 import * as FileSystem from 'expo-file-system';
 import * as SQLite from 'expo-sqlite';
 
-const [db, setDb] = useState(SQLite.openDatabase('PlantsDatabase.db'));
 
-    const exportDb = async () => {
-        if (Platform.OS === "android") {
-          const permissions = await FileSystem.StorageAccessFramework.requestDirectoryPermissionsAsync();
-          if (permissions.granted) {
-            const base64 = await FileSystem.readAsStringAsync(
-              FileSystem.documentDirectory + 'SQLite/PlantsDatabase.db',
-              {
-                encoding: FileSystem.EncodingType.Base64
-              }
-            );
+
     
-            await FileSystem.StorageAccessFramework.createFileAsync(permissions.directoryUri, 'PlantsDatabase.db', 'application/octet-stream')
-            .then(async (uri) => {
-              await FileSystem.writeAsStringAsync(uri, base64, { encoding : FileSystem.EncodingType.Base64 });
-            })
-            .catch((e) => console.log(e));
-          } else {
-            console.log("Permission not granted");
-          }
-        } else {
-          await Sharing.shareAsync(FileSystem.documentDirectory + 'SQLite/PlantsDatabase.db');
-        }
-    }
-    
-    const addPlant = (currentPlant) => {
-        db.transaction(tx => {
-          tx.executeSql('INSERT INTO plants (name, default_interval, sunlight, cycle, edible, poisonous, indoor) values (?)', [currentPlant],
-            (txObj, resultSet) => {},
-            (txObj, error) => console.log(error)
-          );
-        });
-    }
-    
-    useEffect(() => {
-        db.transaction(tx => {
-          tx.executeSql("CREATE TABLE IF NOT EXISTS custom (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT"
-          + "default_interval INTEGER, sunlight TEXT, cycle TEXT, edible INTEGER, poisonous INTEGER, indoor INTEGER)")
-        });
-      }, [db]);
 
 const CustomPlant = () => {
   //const [currentPlant, setCurrentPlant] = useState(undefined);
@@ -55,7 +17,49 @@ const CustomPlant = () => {
   const [edible, setEdible] = useState('');
   const [poisonous, setPoisonous] = useState('');
   const [indoor, setIndoor] = useState('');
+  const [db, setDb] = useState(SQLite.openDatabase('PlantsDatabase.db'));
 
+  const exportDb = async () => {
+    if (Platform.OS === "android") {
+      const permissions = await FileSystem.StorageAccessFramework.requestDirectoryPermissionsAsync();
+      if (permissions.granted) {
+        const base64 = await FileSystem.readAsStringAsync(
+          FileSystem.documentDirectory + 'SQLite/PlantsDatabase.db',
+          {
+            encoding: FileSystem.EncodingType.Base64
+          }
+        );
+
+        await FileSystem.StorageAccessFramework.createFileAsync(permissions.directoryUri, 'PlantsDatabase.db', 'application/octet-stream')
+        .then(async (uri) => {
+          await FileSystem.writeAsStringAsync(uri, base64, { encoding : FileSystem.EncodingType.Base64 });
+        })
+        .catch((e) => console.log(e));
+      } else {
+        console.log("Permission not granted");
+      }
+    } else {
+      await Sharing.shareAsync(FileSystem.documentDirectory + 'SQLite/PlantsDatabase.db');
+    }
+  }
+
+  const addPlant = () => {
+    db.transaction(tx => {
+      tx.executeSql('INSERT INTO custom (name, interval, sunlight, cycle, edible, poisonous, indoor) values (?,?,?,?,?,?,?)', [name, interval, sunlight, cycle, edible, poisonous, indoor],
+        (txObj, resultSet) => {},
+        (txObj, error) => console.log(error)
+      );
+    });
+  }
+
+  useEffect(() => {
+    db.transaction(tx => {
+      //tx.executeSql("DROP TABLE IF EXISTS custom")
+      tx.executeSql("CREATE TABLE IF NOT EXISTS custom (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT,"
+      + "interval INTEGER, sunlight TEXT, cycle TEXT, edible INTEGER, poisonous INTEGER, indoor INTEGER)")
+    });
+  }, [db]);
+  
   return (
     <View style={{padding: 10}}>
       <Text style={{padding: 10, fontSize: 42}}>
@@ -103,7 +107,8 @@ const CustomPlant = () => {
         onChangeText={newIndoor => setIndoor(newIndoor)}
         defaultValue={""}
       />
-      <Button title="Add Plant" onPress={addPlant([name, interval, sunlight, cycle, edible, poisonous, indoor])}/>
+      <Button title="Add Plant" onPress={addPlant}/>
+      <Button title="Export Database" onPress={exportDb}/>
     </View>
   );
 };
