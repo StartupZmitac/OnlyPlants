3dae5e4e107f10714570c16941edd3958eee8029
mikolip716 <mikolip716@student.polsl.pl>
2023-04-17 17:27:05 +0200

    nie działa

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View, TextInput, Button, Platform } from 'react-nativ
 import * as SQLite from 'expo-sqlite';
 import { useState, useEffect } from 'react';
 import * as FileSystem from 'expo-file-system';
+import CustomPlant from './database/CustomPlant.js'
 
 // expo add expo-sqlite
 // expo add expo-file-system
@@ -34,125 +35,11 @@ import * as FileSystem from 'expo-file-system';
 
 
 export default function App() {
-  const [db, setDb] = useState(SQLite.openDatabase('PlantsDatabase.db'));
-  const [isLoading, setIsLoading] = useState(true);
-  const [plants, setPlants] = useState([]);
-  const [currentPlant, setCurrentPlant] = useState(undefined);
-
- 
-
-  const exportDb = async () => {
-    if (Platform.OS === "android") {
-      const permissions = await FileSystem.StorageAccessFramework.requestDirectoryPermissionsAsync();
-      if (permissions.granted) {
-        const base64 = await FileSystem.readAsStringAsync(
-          FileSystem.documentDirectory + 'SQLite/PlantsDatabase.db',
-          {
-            encoding: FileSystem.EncodingType.Base64
-          }
-        );
-
-        await FileSystem.StorageAccessFramework.createFileAsync(permissions.directoryUri, 'PlantsDatabase.db', 'application/octet-stream')
-        .then(async (uri) => {
-          await FileSystem.writeAsStringAsync(uri, base64, { encoding : FileSystem.EncodingType.Base64 });
-        })
-        .catch((e) => console.log(e));
-      } else {
-        console.log("Permission not granted");
-      }
-    } else {
-      await Sharing.shareAsync(FileSystem.documentDirectory + 'SQLite/PlantsDatabase.db');
-    }
-  }
-
-  useEffect(() => {
-    db.transaction(tx => {
-      tx.executeSql('CREATE TABLE IF NOT EXISTS plants (id INTEGER PRIMARY KEY AUTOINCREMENT, plant TEXT)')
-    });
-
-    db.transaction(tx => {
-      tx.executeSql('SELECT * FROM plants', null,
-        (txObj, resultSet) => setPlants(resultSet.rows._array),
-        (txObj, error) => console.log(error)
-      );
-    });
-
-    setIsLoading(false);
-  }, [db]);
-
-  if (isLoading) {
-    return (
-      <View style={styles.container}>
-        <Text>Loading plants...</Text>
-      </View>
-    );
-  }
-
-  const addPlant = () => {
-    db.transaction(tx => {
-      tx.executeSql('INSERT INTO plants (plant) values (?)', [currentPlant],
-        (txObj, resultSet) => {
-          let existingPlants = [...plants];
-          existingPlants.push({ id: resultSet.insertId, plant: currentPlant});
-          setPlants(existingPlants);
-          setCurrentPlant(undefined);
-        },
-        (txObj, error) => console.log(error)
-      );
-    });
-  }
-
-  const deletePlant = (id) => {
-    db.transaction(tx => {
-      tx.executeSql('DELETE FROM plants WHERE id = ?', [id],
-        (txObj, resultSet) => {
-          if (resultSet.rowsAffected > 0) {
-            let existingPlants = [...plants].filter(plant => plant.id !== id);
-            setPlants(existingPlants);
-          }
-        },
-        (txObj, error) => console.log(error)
-      );
-    });
-  };
-
-  const updatePlant = (id) => {
-    db.transaction(tx => {
-      tx.executeSql('UPDATE plants SET plant = ? WHERE id = ?', [currentPlant, id],
-        (txObj, resultSet) => {
-          if (resultSet.rowsAffected > 0) {
-            let existingPlants = [...plants];
-            const indexToUpdate = existingPlants.findIndex(plant => plant.id === id);
-            existingPlants[indexToUpdate].plant = currentPlant;
-            setPlants(existingPlants);
-            setCurrentPlant(undefined);
-          }
-        },
-        (txObj, error) => console.log(error)
-      );
-    });
-  };
-
-  const showPlant = () => {
-    return plants.map((plant, index) => {
-      return (
-        <View key={index} style={styles.row}>
-          <Text>{plant.plant}</Text>
-          <Button title='Delete' onPress={() => deletePlant(plant.id)} />
-          <Button title='Update' onPress={() => updatePlant(plant.id)} />
-        </View>
-      );
-    });
-  };
-
   return (
-    <View style={styles.container}>
-      <TextInput value={currentPlant} placeholder='plant' onChangeText={setCurrentPlant} />
-      <Button title="Add Plant" onPress={addPlant} />
-      {showPlant()}
-      <Button title="Export Db" onPress={exportDb} />
-      <StatusBar style="auto" />
+    <View style = {styles.container}>
+      <CustomPlant />
     </View>
+
   );
 }
 
---
+++ database/CustomPlant.js
@@ -0,0 +1,111 @@
+import React, {useState, useEffect} from 'react';
+import {Text, TextInput, View, Button} from 'react-native';
+import * as FileSystem from 'expo-file-system';
+import * as SQLite from 'expo-sqlite';
+
+const [db, setDb] = useState(SQLite.openDatabase('PlantsDatabase.db'));
+
+    const exportDb = async () => {
+        if (Platform.OS === "android") {
+          const permissions = await FileSystem.StorageAccessFramework.requestDirectoryPermissionsAsync();
+          if (permissions.granted) {
+            const base64 = await FileSystem.readAsStringAsync(
+              FileSystem.documentDirectory + 'SQLite/PlantsDatabase.db',
+              {
+                encoding: FileSystem.EncodingType.Base64
+              }
+            );
+    
+            await FileSystem.StorageAccessFramework.createFileAsync(permissions.directoryUri, 'PlantsDatabase.db', 'application/octet-stream')
+            .then(async (uri) => {
+              await FileSystem.writeAsStringAsync(uri, base64, { encoding : FileSystem.EncodingType.Base64 });
+            })
+            .catch((e) => console.log(e));
+          } else {
+            console.log("Permission not granted");
+          }
+        } else {
+          await Sharing.shareAsync(FileSystem.documentDirectory + 'SQLite/PlantsDatabase.db');
+        }
+    }
+    
+    const addPlant = (currentPlant) => {
+        db.transaction(tx => {
+          tx.executeSql('INSERT INTO plants (name, default_interval, sunlight, cycle, edible, poisonous, indoor) values (?)', [currentPlant],
+            (txObj, resultSet) => {},
+            (txObj, error) => console.log(error)
+          );
+        });
+    }
+    
+    useEffect(() => {
+        db.transaction(tx => {
+          tx.executeSql("CREATE TABLE IF NOT EXISTS custom (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT"
+          + "default_interval INTEGER, sunlight TEXT, cycle TEXT, edible INTEGER, poisonous INTEGER, indoor INTEGER)")
+        });
+      }, [db]);
+
+const CustomPlant = () => {
+  //const [currentPlant, setCurrentPlant] = useState(undefined);
+  const [name, setName] = useState('');
+  //const [watering, setWatering] = useState('');
+  const [interval, setInterval] = useState('');
+  const [sunlight, setSunlight] = useState('');
+  const [cycle, setCycle] = useState('');
+  const [edible, setEdible] = useState('');
+  const [poisonous, setPoisonous] = useState('');
+  const [indoor, setIndoor] = useState('');
+
+  return (
+    <View style={{padding: 10}}>
+      <Text style={{padding: 10, fontSize: 42}}>
+        Add custom plant
+      </Text>
+      <TextInput
+        style={{height: 40}}
+        placeholder="Input plant name:"
+        onChangeText={newName => setName(newName)}
+        defaultValue={""}
+      />
+      <TextInput
+        style={{height: 40}}
+        placeholder="Input watering interval (in days):"
+        onChangeText={newInterval => setInterval(newInterval)}
+        defaultValue={""}
+      />
+      <TextInput
+        style={{height: 40}}
+        placeholder="Input insolation (optional):"
+        onChangeText={newSun => setSunlight(newSun)}
+        defaultValue={""}
+      />
+      <TextInput
+        style={{height: 40}}
+        placeholder="Input plant cycle (optional):"
+        onChangeText={newCycle => setCycle(newCycle)}
+        defaultValue={""}
+      />
+      <TextInput
+        style={{height: 40}}
+        placeholder="Input plant edibility (optional):"
+        onChangeText={newEdible => setEdible(newEdible)}
+        defaultValue={""}
+      />
+      <TextInput
+        style={{height: 40}}
+        placeholder="Is the plant poisonous?:"
+        onChangeText={newPoison => setPoisonous(newPoison)}
+        defaultValue={""}
+      />
+      <TextInput
+        style={{height: 40}}
+        placeholder="Is the plan planted indoor?:"
+        onChangeText={newIndoor => setIndoor(newIndoor)}
+        defaultValue={""}
+      />
+      <Button title="Add Plant" onPress={addPlant([name, interval, sunlight, cycle, edible, poisonous, indoor])}/>
+    </View>
+  );
+};
+
+export default CustomPlant;
