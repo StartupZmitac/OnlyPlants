8447354309c7363f6784acdf6f5db18763e25f3b
Aleksandra Kyc <ola.kyc@gmail.com>
2023-05-06 14:54:15 +0200

    beautiful working database.

--- src/components/CustomPlant/CustomPlant.js
+++ src/components/CustomPlant/CustomPlant.js
@@ -50,26 +50,24 @@ const CustomPlant = () => {
   }
 
   useEffect(() => {
-    // db.transaction(tx => {
-    //   //tx.executeSql("DROP TABLE IF EXISTS custom")
-    //   tx.executeSql("CREATE TABLE IF NOT EXISTS custom (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT,"
-    //   + "interval INTEGER, sunlight TEXT, cycle TEXT, edible INTEGER, poisonous INTEGER, indoor INTEGER)")
-    // });
-    db.exec([{ sql: 'PRAGMA foreign_keys = ON;', args: [] }], false, () =>
-    console.log('Foreign keys turned on')
-  );    //tx.executeSql("PRAGMA foreign_keys = ON;");
-
-
+    
+    db._db.exec(
+      [{ sql: 'PRAGMA foreign_keys = ON;', args: [] }],
+      false,
+      () => console.log('Foreign keys turned on'),
+    );
+  
     db.transaction(tx => {
-      //tx.executeSql("DROP TABLE IF EXISTS watering");
-      //tx.executeSql("PRAGMA foreign_keys = ON;");
-      tx.executeSql("CREATE TABLE IF NOT EXISTS watering (id INTEGER PRIMARY KEY, name TEXT NOT NULL, interval INTEGER NOT NULL);");
-      tx.executeSql("CREATE TABLE IF NOT EXISTS location (id INTEGER PRIMARY KEY, name TEXT NOT NULL, location TEXT NOT NULL);");
-      tx.executeSql("CREATE TABLE IF NOT EXISTS group (id INTEGER PRIMARY KEY, name TEXT NOT NULL, location_id INTEGER NOT NULL, FOREIGN KEY (location_id) REFERENCES location (id));");
-      // tx.executeSql("CREATE TABLE IF NOT EXISTS user_info (id INTEGER PRIMARY KEY, pet INTEGER NOT NULL, username TEXT NOT NULL);");
-      // tx.executeSql("CREATE TABLE IF NOT EXISTS plants ( id INTEGER PRIMARY KEY, name TEXT NOT NULL, sunlight INTEGER NOT NULL, cycle INTEGER NOT NULL, edible INTEGER NOT NULL, poisonous INTEGER NOT NULL, indoor INTEGER NOT NULL, custom INTEGER NOT NULL, watering INTEGER NOT NULL, FOREIGN KEY (watering) REFERENCES watering(id));");
-      // tx.executeSql("CREATE TABLE IF NOT EXISTS planted(id INTEGER PRIMARY KEY, date_planted TEXT NOT NULL, date_watered TEXT NOT NULL, date_notified TEXT NOT NULL, interval INTEGER NOT NULL, custom_name TEXT NOT NULL, inside INTEGER NOT NULL, plant_id INTEGER NOT NULL, group_id INTEGER NOT NULL, location_id INTEGER NOT NULL, FOREIGN KEY (plant_id) REFERENCES plants(id), FOREIGN KEY (group_id) REFERENCES group(id), FOREIGN KEY (location_id) REFERENCES location(id));");
+      
+      tx.executeSql("CREATE TABLE IF NOT EXISTS watering( watering_id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR NOT NULL, interval INTEGER NOT NULL);");
+      tx.executeSql("CREATE TABLE IF NOT EXISTS location( location_id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR NOT NULL, location VARCHAR NOT NULL);");
+      tx.executeSql("CREATE TABLE IF NOT EXISTS groups( group_id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR NOT NULL, first_name VARCHAR, location_id INTEGER, CONSTRAINT fk_location FOREIGN KEY (location_id) REFERENCES location(location_id));");
+      tx.executeSql("CREATE TABLE IF NOT EXISTS user_info (id INTEGER PRIMARY KEY, pet INTEGER NOT NULL, username TEXT NOT NULL);");
+      tx.executeSql("CREATE TABLE IF NOT EXISTS plants ( id INTEGER PRIMARY KEY, name TEXT NOT NULL, sunlight INTEGER NOT NULL, cycle INTEGER NOT NULL, edible INTEGER NOT NULL, poisonous INTEGER NOT NULL, indoor INTEGER NOT NULL, custom INTEGER NOT NULL, watering INTEGER NOT NULL, FOREIGN KEY (watering) REFERENCES watering(id));");
+      tx.executeSql("CREATE TABLE IF NOT EXISTS planted(id INTEGER PRIMARY KEY, date_planted TEXT NOT NULL, date_watered TEXT NOT NULL, date_notified TEXT NOT NULL, interval INTEGER NOT NULL, custom_name TEXT NOT NULL, inside INTEGER NOT NULL, plant_id INTEGER NOT NULL, group_id INTEGER NOT NULL, location_id INTEGER NOT NULL, FOREIGN KEY (plant_id) REFERENCES plants(id), FOREIGN KEY (group_id) REFERENCES groups(id), FOREIGN KEY (location_id) REFERENCES location(id));");
+    
     });
+
   }, [db]);
   
   return (
