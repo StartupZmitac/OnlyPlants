bff7a8958d43a660353ec2b8527380fa965ecb98
wiktkaj492 <72332485+wiktkaj492@users.noreply.github.com>
2023-05-17 15:55:20 +0200

    test

--- src/components/Mainpage/Mainpage.js
+++ src/components/Mainpage/Mainpage.js
@@ -7,7 +7,23 @@ const Mainpage = () => {
     return (
         //Pomiędzy tagami NativeBaseProvider piszesz cały kod który jest wyświetlany.
         <NativeBaseProvider>
-            
+        <Box>
+        <Box style={styles.mainBody}>
+          <Box style={styles.checkBox}>
+          <CheckBox checked={true} color="green"/>
+            <Column space={4} alignItems="center">
+            <Input 
+              variant="rounded"
+              placeholder="Flower 1"
+              //onChangeText={newName => setName(newName)}
+              placeholderTextColor="#F7F6DC"
+              defaultValue={""}
+              fontSize={'20'} 
+            />
+            </Column>
+            </Box>
+      </Box>
+      </Box>
         </NativeBaseProvider>
       );
 
---
+++ src/components/Mainpage/Mainpage.style.js
@@ -0,0 +1,29 @@
+import { StyleSheet } from 'react-native';
+export default StyleSheet.create({
+    mainBody: {
+        backgroundColor: '#B1D7B4',
+        alignItems: 'center',
+        paddingTop: '10%',
+        height: '100%'
+      },
+      choiceBox: {
+        backgroundColor: '#F7F6DC',
+        height: '78%',
+        width: '80%',
+        alignItems: 'center',
+        borderRadius: 50,
+        padding: '5%'
+      },
+      inputField:{
+        backgroundColor: '#FFC090',
+        width: '60%',
+        height: '100%',
+      },
+      titleBox: {
+        backgroundColor: '#7FB77E',
+        alignItems: 'center'
+      },
+      checkbox: {
+        
+      }
+  });
