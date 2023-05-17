d0e15c19c85c559ff693ab3f24bda47a9c483d4c
mikolip716 <mikolip716@student.polsl.pl>
2023-05-17 11:57:35 +0200

    updated input fields

--- src/components/CustomPlant/CustomPlant.js
+++ src/components/CustomPlant/CustomPlant.js
@@ -1,7 +1,9 @@
 import React, {useState, useEffect} from 'react';
+import {TouchableWithoutFeedback, Keyboard} from 'react-native';
+import {Box, Button, NativeBaseProvider, Heading, Input, Column, Row, Select} from "native-base"
+import { Entypo } from '@expo/vector-icons'; 
 import styles from './CustomPlant.style.js'
-import {Box, Button, NativeBaseProvider, Heading, Input, Column, Row} from "native-base"
-import { database } from '../../database/PlantsDb.js';
+import { database } from '../../database/PlantsDb.js'
 
 const CustomPlant = () => {
   const [name, setName] = useState('');
@@ -27,79 +29,128 @@ const CustomPlant = () => {
   
   return (
     <NativeBaseProvider>
-      <Box>
-        <Box style={styles.mainBody}>
-          <Box style={styles.choiceBox}>
-            <Column space={4} alignItems="center">
-            <Input 
-              variant="rounded"
-              placeholder="Input plant name:"
-              onChangeText={newName => setName(newName)}
-              placeholderTextColor="#F7F6DC"
-              defaultValue={""}
-              fontSize={'20'} 
-              style={styles.inputField}
-            />
-             <Input 
-              variant="rounded"
-              placeholder="Input watering interval (in days):"
-              onChangeText={newInterval => setInterval(newInterval)}
-              placeholderTextColor="#F7F6DC"
-              defaultValue={""}
-              fontSize={'20'} 
-              style={styles.inputField}
-            />
-            <Input 
-              variant="rounded"
-              placeholder="Input insolation (optional):"
-              onChangeText={newSun => setSunlight(newSun)}
-              defaultValue={""}
-              placeholderTextColor="#F7F6DC"
-              fontSize={'20'} 
-              style={styles.inputField} />
-            <Input 
-              variant="rounded"
-              placeholder="Input plant cycle (optional):"
-              onChangeText={newCycle => setCycle(newCycle)}
-              defaultValue={""}
-              placeholderTextColor="#F7F6DC"
-              fontSize={'20'} 
-              style={styles.inputField} />
-            <Input 
-              variant="rounded"
-              placeholder="Input plant edibility (optional):"
-              onChangeText={newEdible => setEdible(newEdible)}
-              defaultValue={""}
-              placeholderTextColor="#F7F6DC"
-              fontSize={'20'} 
-              style={styles.inputField} />
-            <Input 
-              variant="rounded"
-              placeholder="Is the plant poisonous?:"
-              onChangeText={newPoison => setPoisonous(newPoison)}
-              defaultValue={""}
-              placeholderTextColor="#F7F6DC"
-              fontSize={'20'} 
-              style={styles.inputField} />
-            <Input 
-              variant="rounded"
-              placeholder="Is the plan planted indoor?:"
-              onChangeText={newIndoor => setIndoor(newIndoor)}
-              defaultValue={""}
-              placeholderTextColor="#F7F6DC"
-              fontSize={'20'} 
-              style={styles.inputField} />
-            </Column>
+      <TouchableWithoutFeedback onPress = {() => {Keyboard.dismiss();}}>
+        <Box>
+          <Box style={styles.mainBody}>
+            <Box style={styles.choiceBox}>
+              <Column space={4} alignItems="center">
+              <Input 
+                variant="rounded"
+                placeholder="Plant name"
+                onChangeText={newName => setName(newName)}
+                placeholderTextColor="#F7F6DC"
+                defaultValue={""}
+                fontSize={'20'} 
+                style={styles.inputField}
+              />
+              <Input 
+                variant="rounded"
+                placeholder="Watering interval (in days)"
+                onChangeText={newInterval => setInterval(newInterval)}
+                placeholderTextColor="#F7F6DC"
+                defaultValue={""}
+                fontSize={'20'} 
+                inputMode='numeric'
+                style={styles.inputField}
+              />
+              <Select selectedValue={sunlight}
+                minWidth="290"
+                accessibilityLabel="Insolation"
+                placeholder="Insolation"
+                placeholderTextColor="#F7F6DC"
+                backgroundColor="#FFC090"
+                variant="rounded"
+                color="#F7F6DC"
+                fontSize={'20'}
+                dropdownIcon={<Entypo name="chevron-small-down" size={24} color="#F7F6DC"/>}
+                _selectedItem={{
+                bg: "#FFC090"
+                }} mt={1} onValueChange={itemValue => setSunlight(itemValue)}>
+                  <Select.Item label="Full shade" value="0" />
+                  <Select.Item label="Part shade" value="1" />
+                  <Select.Item label="Patially in sun" value="2" />
+                  <Select.Item label="Fully in sun" value="3" />
+              </Select>
+              <Select selectedValue={cycle}
+                minWidth="290"
+                accessibilityLabel="Plant cycle"
+                placeholder="Plant cycle"
+                placeholderTextColor="#F7F6DC"
+                backgroundColor="#FFC090"
+                variant="rounded"
+                color="#F7F6DC"
+                fontSize={'20'}
+                dropdownIcon={<Entypo name="chevron-small-down" size={24} color="#F7F6DC"/>}
+                _selectedItem={{
+                bg: "#FFC090"
+                }} mt={1} onValueChange={itemValue => setCycle(itemValue)}>
+                  <Select.Item label="Perennial" value="0" />
+                  <Select.Item label="Annual" value="1" />
+                  <Select.Item label="Biennial" value="2" />
+                  <Select.Item label="Biannual" value="3" />
+              </Select>
+              <Select selectedValue={poisonous}
+                minWidth="290"
+                accessibilityLabel="Is the plant edible?"
+                placeholder="Is the plant edible?"
+                placeholderTextColor="#F7F6DC"
+                backgroundColor="#FFC090"
+                variant="rounded"
+                color="#F7F6DC"
+                fontSize={'20'}
+                dropdownIcon={<Entypo name="chevron-small-down" size={24} color="#F7F6DC"/>}
+                _selectedItem={{
+                bg: "#FFC090"
+                }} mt={1} onValueChange={itemValue => setEdible(itemValue)}>
+                  <Select.Item label="Edible" value="1" />
+                  <Select.Item label="Inedible" value="0" />
+              </Select>
+              <Select selectedValue={poisonous}
+                minWidth="290"
+                accessibilityLabel="Is the plant poisonous?"
+                placeholder="Is the plant poisonous?"
+                placeholderTextColor="#F7F6DC"
+                backgroundColor="#FFC090"
+                variant="rounded"
+                color="#F7F6DC"
+                fontSize={'20'}
+                dropdownIcon={<Entypo name="chevron-small-down" size={24} color="#F7F6DC"/>}
+                _selectedItem={{
+                bg: "#FFC090"
+                }} mt={1} onValueChange={itemValue => setPoisonous(itemValue)}>
+                  <Select.Item label="Poisonous" value="1" />
+                  <Select.Item label="Not poisonous" value="0" />
+              </Select>
+              <Select selectedValue={indoor}
+                minWidth="290"
+                accessibilityLabel="Is the plant indoor?"
+                placeholder="Is the plant indoor?"
+                placeholderTextColor="#F7F6DC"
+                backgroundColor="#FFC090"
+                variant="rounded"
+                color="#F7F6DC"
+                fontSize={'20'}
+                dropdownIcon={<Entypo name="chevron-small-down" size={24} color="#F7F6DC"/>}
+                _selectedItem={{
+                bg: "#FFC090"
+                }} mt={1} onValueChange={itemValue => setIndoor(itemValue)}>
+                  <Select.Item label="Indoor" value="1" />
+                  <Select.Item label="Outdoor" value="0" />
+              </Select>
+              </Column>
+            </Box>
+            <Row style={{alignItems: 'center', padding: '10%'}}>
+            <Button size="lg" style={{backgroundColor: '#FFC090', color: "#F7F6DC"}}>Add Plant</Button>
+           
+            </Row>
           </Box>
-          <Row style={{alignItems: 'center', padding: '10%'}}>
-          <Button size="lg" style={{backgroundColor: '#FFC090', color: "#F7F6DC", marginRight: '10%'}}>Add Plant</Button>
-          <Button size="lg" style={styles.button}> Export </Button>
-          <Button size="lg" style={styles.button}> Drop </Button>
-          </Row>
         </Box>
-      </Box>
+      </TouchableWithoutFeedback>
     </NativeBaseProvider>
   );
 };
-
+/*
+  <Button size="lg" style={styles.button}> Export </Button>
+  <Button size="lg" style={styles.button}> Drop </Button>
+*/
 export default CustomPlant;
