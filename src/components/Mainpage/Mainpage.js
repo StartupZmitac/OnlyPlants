2a6c01b7de7c2e5da4c1c49236d28653795e871c
Michał Niedbalski <michnie507@student.polsl.pl>
2023-06-28 15:01:24 +0200

    Changed day change icons and corrected logic

--- src/components/Mainpage/Mainpage.js
+++ src/components/Mainpage/Mainpage.js
@@ -2,7 +2,7 @@ import React, { useRef, useEffect, useState } from "react";
 import { Animated, Easing } from 'react-native'
 import { NativeBaseProvider, Text, Icon, useToast, Box, Row, Button, Heading, Column, Checkbox } from "native-base";
 import styles from './Mainpage.style.js'
-import { Entypo } from '@expo/vector-icons';
+import { AntDesign, Entypo } from '@expo/vector-icons';
 
 const MainPage = () => {
     const [counter, setCounter] = useState(0);
@@ -21,20 +21,22 @@ const MainPage = () => {
       };
 
     const [checkboxes] = useState([
-        { id: 1, name: 'Flower 1 (26.06)', checked: false, day: 26, month: 6, year: 2023 },
-        { id: 2, name: 'Flower 2 (26.06)', checked: false, day: 26, month: 6, year: 2023 },
-        { id: 3, name: 'Flower 3 (26.06)', checked: false, day: 26, month: 6, year: 2023 },
-        { id: 4, name: 'Flower 4 (26.06)', checked: false, day: 26, month: 6, year: 2023 },
-        { id: 5, name: 'Flower 1 (27.06)', checked: false, day: 27, month: 6, year: 2023 },
-        { id: 6, name: 'Flower 2 (27.06)', checked: false, day: 27, month: 6, year: 2023 },
-        { id: 7, name: 'Flower 1 (28.06)', checked: false, day: 28, month: 6, year: 2023 },
-        { id: 8, name: 'Flower 2 (28.06)', checked: false, day: 28, month: 6, year: 2023 },
+        { id: 1, name: 'Flower 1 (28.06)', checked: false, day: 28, month: 6, year: 2023 },
+        { id: 2, name: 'Flower 2 (28.06)', checked: false, day: 28, month: 6, year: 2023 },
+        { id: 3, name: 'Flower 3 (28.06)', checked: false, day: 28, month: 6, year: 2023 },
+        { id: 4, name: 'Flower 4 (28.06)', checked: false, day: 28, month: 6, year: 2023 },
+        { id: 5, name: 'Flower 1 (29.06)', checked: false, day: 29, month: 6, year: 2023 },
+        { id: 6, name: 'Flower 2 (29.06)', checked: false, day: 29, month: 6, year: 2023 },
+        { id: 7, name: 'Flower 1 (30.06)', checked: false, day: 30, month: 6, year: 2023 },
+        { id: 8, name: 'Flower 2 (30.06)', checked: false, day: 30, month: 6, year: 2023 },
     ]);
 
 
     const getDayName = () => {
-        var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-        return days[new Date().getDay()+counter];
+        const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+        const index = (new Date().getDay() + (counter % 7) + 7) % 7;
+        console.log(days[index]);
+        return days[index];
     }
 
 
@@ -51,14 +53,14 @@ const MainPage = () => {
         <NativeBaseProvider>
             <Box style={styles.mainBody}>
                 <Row>
-                    <Button style={{bottom: '25%', marginRight: '5%'}} onPress={clickLeft}>
-                        L
+                    <Button style={{bottom: '25%', marginRight: '5%', backgroundColor: '#FFC090', borderRadius: 50}} onPress={clickLeft}>
+                        <AntDesign name="leftcircleo" size={24} color="#FFFFFF" />
                     </Button>
                     <Box style={styles.dayHeader}>
                         <Text bold fontSize="3xl" style={{ color: '#ffffff' }}>{getDayName()}</Text>
                     </Box>
-                    <Button style={{bottom: '25%', marginLeft: '5%'}} onPress={clickRight}>
-                        R
+                    <Button style={{bottom: '25%', marginLeft: '5%', backgroundColor: '#FFC090', borderRadius: 50}} onPress={clickRight}>
+                        <AntDesign name="rightcircleo" size={24} color="#FFFFFF" />
                     </Button>
                 </Row>
 
