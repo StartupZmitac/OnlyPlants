240b303607ae7a8f528419cfdaf9de6df71597e8
mikolip716 <mikolip716@student.polsl.pl>
2023-06-30 15:41:54 +0200

    remove plant functional, added manual refresh

--- src/components/MyPlantsManagement/MyPlantsManagement.js
+++ src/components/MyPlantsManagement/MyPlantsManagement.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
+import { RefreshControl } from 'react-native';
 import styles from './MyPlantsManagement.style.js'
-import { Box, Button, NativeBaseProvider, Modal, Text, Row, Blur, Heading, View, ScrollView, Center, VStack, Column, Image } from "native-base"
-
+import { Box, Button, NativeBaseProvider, Modal, Text, Row, Blur, Heading, View, ScrollView, Center, VStack, Column, Image, useToast } from "native-base"
+import { selectPlanted, deletePlanted } from '../../database/PlantsDb.js'
 
 const MyPlantsManagement = () => {
     const [selectedPlant, setSelectedPlant] = useState('');
+    /*
     const [plants] = useState([
         { key: 1, name: 'Plant 1', day: 28, month: 6, year: 2023 },
         { key: 2, name: 'Plant 2', day: 28, month: 6, year: 2023 },
@@ -18,7 +20,19 @@ const MyPlantsManagement = () => {
         { key: 10, name: 'Plant 10', day: 1, month: 7, year: 2023 },
         { key: 11, name: 'Plant 11', day: 1, month: 7, year: 2023 }
     ]);
+    */
+    const [plants, setPlants] = useState([]);
     const [isModalOpen, setIsModalOpen] = useState(false);
+    const [refreshing, setRefreshing] = React.useState(false);
+    const toast = useToast();
+    
+    const onRefresh = React.useCallback(() => {
+      setRefreshing(true);
+      selectPlanted(setAndParsePlantList);
+      setTimeout(() => {
+        setRefreshing(false);
+      }, 2000);
+    }, []);  
 
     const handleOpenModal = (plant) => {
         setSelectedPlant(plant);
@@ -29,11 +43,52 @@ const MyPlantsManagement = () => {
         setIsModalOpen(false);
     };
 
+    function setAndParsePlantList(resultSet){
+        var options = []
+        for (let i = 0; i < resultSet.length; i++) {
+            temp = JSON.stringify(resultSet.at(i));
+            parsed = JSON.parse(temp);
+            console.log(parsed)
+            date_watered = new Date(parsed.date_watered)
+            date_watered.setDate(date_watered.getDate()+parsed.interval)
+            console.log(date_watered)
+            options.push({key: parsed.id, name: parsed.custom_name, checked: false, day: date_watered.getDate(), month: date_watered.getMonth()+1, year: date_watered.getFullYear()});
+            console.log("row: ", options[i]);
+        }
+        setPlants(options);
+    }
+
+    function manageGroups()
+    {
+        //todo: implement function
+        console.log("groups managed")
+    }
+
+    function changeName()
+    {
+        //todo: implement function
+        console.log("changed name")
+    }
+
+    function deletePlant(plant)
+    {
+        deletePlanted(plant.key, () => {console.log("deleted")});
+        toast.show({
+            description: `Deleted ${plant.name}`
+        });
+    }
+
+    useEffect(() => {
+        selectPlanted(setAndParsePlantList);
+    }, []);
+
     return (
         <NativeBaseProvider>
             <Box style={styles.mainBody}>
                 <Box style={styles.choiceBox}>
-                    <ScrollView w="200" h="80">
+                    <ScrollView w="200" h="80" refreshControl={
+                        <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+                    }>
                         <VStack flex="1">
                             {plants.map((item) => (
                                 <View key={item.key}>
@@ -56,13 +111,13 @@ const MyPlantsManagement = () => {
                             </Column>
                         </Box>
                         <Column style={{ paddingTop: '20%' }}>
-                            <Button size="lg" style={{ ...styles.button, marginBottom: '10%' }}>
+                            <Button size="lg" style={{ ...styles.button, marginBottom: '10%' }} onPress={changeName}>
                                 Change name
                             </Button>
-                            <Button size="lg" style={{ ...styles.button, marginBottom: '10%' }}>
+                            <Button size="lg" style={{ ...styles.button, marginBottom: '10%' }} onPress={manageGroups}>
                                 Manage groups
                             </Button>
-                            <Button size="lg" style={styles.button}>
+                            <Button size="lg" style={styles.button} onPress={() => deletePlant(selectedPlant)}>
                                 Remove plant
                             </Button>
                         </Column>
