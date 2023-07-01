02e6f405cc5fe670fa18c56099fa3fb47d6fe121
Szymon Moc <72348203+Szymmoc895@users.noreply.github.com>
2023-07-01 22:03:53 +0200

    important update

--- important.asm
+++ important.asm
@@ -14,4 +14,4 @@ _start:
         xor     %rdi, %rdi              # we want return code 0
         syscall                         # invoke operating system to exit
 message:
-        .ascii  "Hello, world\n"
+        .ascii  "I hate JS\n"
