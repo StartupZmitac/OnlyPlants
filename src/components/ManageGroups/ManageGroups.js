9e7897d1c5ae4e67a6c6f92f888ed31cb13a358b
Michał Niedbalski <michnie507@student.polsl.pl>
2023-07-02 21:05:28 +0200

    Manage groups front finished without adding group

--- src/components/ManageGroups/ManageGroups.js
+++ src/components/ManageGroups/ManageGroups.js
@@ -1,13 +1,15 @@
-import React, { useState, useEffect } from 'react';
-import styles from './ManageGroups.style.js'
+import React, { useState } from 'react';
 import { TouchableWithoutFeedback, Keyboard } from 'react-native';
-import { Box, Button, Modal, NativeBaseProvider,Input, Text, Row, Column, Image, ScrollView } from "native-base"
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Box, Button, Modal, NativeBaseProvider, Input, Text, Row, Column, ScrollView, } from 'native-base';
+import styles from './ManageGroups.style.js';
 
 const ManageGroups = ({ route }) => {
     const [isModalOpen, setIsModalOpen] = useState(false);
+    const [isSecondModalOpen, setIsSecondModalOpen] = useState(false);
     const [selectedGroup, setSelectedGroup] = useState('');
-    const [testPlants] = useState([
+    const [selectedPlant, setSelectedPlant] = useState('');
+
+    const testPlants = [
         { id: 1, name: 'Plant 1' },
         { id: 2, name: 'Plant 2' },
         { id: 3, name: 'Plant 3' },
@@ -17,12 +19,14 @@ const ManageGroups = ({ route }) => {
         { id: 7, name: 'Plant 7' },
         { id: 8, name: 'Plant 8' },
         { id: 9, name: 'Plant 9' },
-    ]);
-    const [testGroups] = useState([
+    ];
+
+    const testGroups = [
         { id: 1, name: 'Group 1', plants: [] },
         { id: 2, name: 'Group 2', plants: [] },
         { id: 3, name: 'Group 3', plants: [] },
-    ]);
+    ];
+
     const handleOpenModal = (group) => {
         setSelectedGroup(group);
         setIsModalOpen(true);
@@ -31,54 +35,76 @@ const ManageGroups = ({ route }) => {
     const handleCloseModal = () => {
         setIsModalOpen(false);
     };
+
+    const handleOpenSecondModal = (plant) => {
+        setSelectedPlant(plant);
+        setIsSecondModalOpen(true);
+    };
+
+    const handleCloseSecondModal = () => {
+        setIsSecondModalOpen(false);
+    };
+
     return (
         <NativeBaseProvider>
-            <TouchableWithoutFeedback onPress={() => { Keyboard.dismiss(); }}>
+            <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
                 <Box style={styles.mainBody}>
                     <Box style={styles.choiceBox}>
                         <Column style={styles.plantColumn}>
                             {testGroups.map((group) => (
-                                <Box key={group.id} style={{
-                                    flexDirection: "row",
-                                    alignItems: "center",
-                                    paddingBottom: '5%',
-                                }}>
-                                    <Button style={styles.inputField} onPress={() => handleOpenModal(group)}>
-                                        <Text bold style={styles.label}>{group.name}</Text>
+                                <Box
+                                    key={group.id}
+                                    style={{
+                                        flexDirection: 'row',
+                                        alignItems: 'center',
+                                        paddingBottom: '5%',
+                                    }}
+                                >
+                                    <Button
+                                        style={styles.inputField}
+                                        onPress={() => handleOpenModal(group)}
+                                    >
+                                        <Text bold style={styles.label}>
+                                            {group.name}
+                                        </Text>
                                     </Button>
                                 </Box>
                             ))}
                         </Column>
                     </Box>
                     <Row style={{ alignItems: 'center', padding: '10%' }}>
-                        <Button size="lg" style={styles.button}>Add group</Button>
+                        <Button size="lg" style={styles.button}>
+                            Add group
+                        </Button>
                     </Row>
                     <Modal borderRadius={50} isOpen={isModalOpen} onClose={handleCloseModal}>
-                        <Modal.Content style={{ height: '60%', backgroundColor: '#F7F6DC', padding: '5%' }}>
+                        <Modal.Content style={{ height: '60%', backgroundColor: '#F7F6DC', padding: '5%', }}>
                             <Column>
-                                    <Input
-                                        variant="rounded"
-                                        placeholder={selectedGroup.name}
-                                        onChangeText={newName => setCustomName(newName)}
-                                        placeholderTextColor="#F7F6DC"
-                                        color="#F7F6DC"
-                                        defaultValue={selectedGroup.name}
-                                        fontSize={'18'}
-                                        style={styles.inputField}
-                                        marginBottom={'8%'}
-                                    />
-                                <Box style={{ backgroundColor: '#B1D7B4', borderRadius: 50, padding: '7%' }}>
-                                    <ScrollView w={['100%', '100%']} h='73%'>
+                                <Input
+                                    variant="rounded"
+                                    placeholder={selectedGroup.name}
+                                    onChangeText={(newName) => setCustomName(newName)}
+                                    placeholderTextColor="#F7F6DC"
+                                    color="#F7F6DC"
+                                    defaultValue={selectedGroup.name}
+                                    fontSize={18}
+                                    style={styles.inputField}
+                                    marginBottom="8%"
+                                />
+                                <Box style={{ backgroundColor: '#B1D7B4', borderRadius: 50, padding: '7%', }}>
+                                    <ScrollView w="100%" h="73%">
                                         {testPlants.map((plant) => (
-                                            <Button key={plant.id} style={{ backgroundColor: '#7FB77E', borderRadius: 50, marginBottom: '5%' }}>
-                                                <Text bold style={styles.label}>{plant.name}</Text>
+                                            <Button key={plant.id} style={{ backgroundColor: '#7FB77E', borderRadius: 50, marginBottom: '5%', }} onPress={() => handleOpenSecondModal(plant)}>
+                                                <Text bold style={styles.label}>
+                                                    {plant.name}
+                                                </Text>
                                             </Button>
                                         ))}
                                     </ScrollView>
                                 </Box>
                             </Column>
-                            <Row style={{ position: 'absolute', bottom: '5%', width: '100%', left: '8%' }}>
-                                <Button size="lg" style={{ ...styles.button, marginRight: '10%', width: '45%' }}>
+                            <Row style={{ position: 'absolute', bottom: '5%', width: '100%', left: '8%', }}>
+                                <Button size="lg" style={{ ...styles.button, marginRight: '10%', width: '45%' }} >
                                     Save
                                 </Button>
                                 <Button size="lg" style={{ ...styles.button, width: '45%' }}>
@@ -87,8 +113,22 @@ const ManageGroups = ({ route }) => {
                             </Row>
                         </Modal.Content>
                     </Modal>
+                    <Modal borderRadius={50} isOpen={isSecondModalOpen} onClose={handleCloseSecondModal}>
+                        <Modal.Content style={{ height: '20%', backgroundColor: '#F7F6DC', padding: '5%', }}>
+                            <Box style={styles.infoBox}>
+                                <Text bold style={styles.label}>Plant name: {selectedPlant.name}</Text>
+                            </Box>
+                            <Row style={{ position: 'absolute', bottom: '10%', width: '100%', left: '8%', }}>
+                                <Button size="lg" style={{ ...styles.button, marginRight: '10%', width: '45%' }}>
+                                    Modify
+                                </Button>
+                                <Button size="lg" style={{ ...styles.button, width: '45%' }}>
+                                    Delete
+                                </Button>
+                            </Row>
+                        </Modal.Content>
+                    </Modal>
                 </Box>
-
             </TouchableWithoutFeedback>
         </NativeBaseProvider>
     );
